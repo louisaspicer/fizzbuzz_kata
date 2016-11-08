970ff152faa526bd5779affe500d16faf38371a2
Louisa <louisaspicer@gmail.com>
2016-11-08 10:39:10 +0000

    deleted contents

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,15 @@
-def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    "Fizzbuzz"
-  elsif number % 3 == 0
-    "Fizz"
-  elsif number % 5 == 0
-    "Buzz"
-  else
-    number
+class Fizzbuzz
+
+  def fizzbuzz(number)
+    if number % 3 == 0 && number % 5 == 0
+      "Fizzbuzz"
+    elsif self % 3 == 0
+      "Fizz"
+    elsif self % 5 == 0
+      "Buzz"
+    else
+      self
+    end
   end
+
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,7 +3,7 @@ require "fizzbuzz.rb"
 describe "fizzbuzz" do
 
   it "should return fizz when given 3" do
-    expect(fizzbuzz(3)).to eq "Fizz"
+    expect(3.fizzbuzz).to eq "Fizz"
   end
 
   it "should return fizz when given a multiple of 3" do
