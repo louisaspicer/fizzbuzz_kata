fb0606015e6270b3db440772f6276e92f6fea7e5
Louisa <louisaspicer@gmail.com>
2016-11-08 10:31:19 +0000

    passes all tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,1 +1,24 @@
-finished
+require "fizzbuzz.rb"
+
+describe "fizzbuzz" do
+
+  it "should return fizz when given 3" do
+    expect(fizzbuzz(3)).to eq "Fizz"
+  end
+
+  it "should return fizz when given a multiple of 3" do
+    expect(fizzbuzz(6)).to eq "Fizz"
+  end
+
+  it "should return buzz when given 5" do
+    expect(fizzbuzz(5)).to eq "Buzz"
+  end
+
+  it "should return buzz when given a multiple of 5" do
+    expect(fizzbuzz(10)).to eq "Buzz"
+  end
+
+  it "should return fizzbuzz when given a multiple of both 3 and 5" do
+    expect(fizzbuzz(15)).to eq "Fizzbuzz"
+  end
+end
