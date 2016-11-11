519daf2b50e3896988c34b043f827e178c72b3ca
Louisa <louisaspicer@gmail.com>
2016-11-11 09:12:38 +0000

    completed kata

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,1 +1,27 @@
-finished
+def is_divisible_by_3?(number)
+  number % 3 == 0
+end
+
+def is_divisible_by_5?(number)
+  number % 5 == 0
+end
+
+def is_divisible_by_3_and_5?(number)
+  number % 3 == 0 && number % 5 == 0
+end
+
+def is_divisible_by?(divisor, number)
+  number % divisor == 0
+end
+
+def fizzbuzz(number)
+  if is_divisible_by?(3, number) && is_divisible_by?(5, number)
+    "Fizzbuzz"
+  elsif is_divisible_by?(3, number)
+    "Fizz"
+  elsif is_divisible_by?(5, number)
+    "Buzz"
+  else
+    number
+  end
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,1 +1,51 @@
-finished
+require './lib/fizzbuzz.rb'
+
+describe 'fizzbuzz' do
+  it "should test for divisibility by 3" do
+    expect(is_divisible_by_3?(3)).to eq true
+  end
+
+  it "should test for indivisibility by 3" do
+    expect(is_divisible_by_3?(4)).to eq false
+  end
+
+  it "should test for divisibility by 5?" do
+    expect(is_divisible_by_5?(5)).to eq true
+  end
+
+  it "should test for indivisibilty by 5" do
+    expect(is_divisible_by_5?(6)).to eq false
+  end
+
+  it "should test for divisibility by 3 & 5" do
+    expect(is_divisible_by_3_and_5?(15)).to eq true
+  end
+
+  it "should test for indivisibility by 3 & 5" do
+    expect(is_divisible_by_3_and_5?(16)).to eq false
+  end
+
+  it "should test for arbitrary divisibility" do
+    expect(is_divisible_by?(3, 3)).to eq true
+  end
+
+  it "should test for arbitrary divisibility" do
+    expect(is_divisible_by?(3, 4)).to eq false
+  end
+
+  it "should return fizz when given a multiple of 3" do
+    expect(fizzbuzz(3)).to eq "Fizz"
+  end
+
+  it "should return buzz when given a multiple of 5" do
+    expect(fizzbuzz(5)).to eq "Buzz"
+  end
+
+  it "should return fizzbuzz when given a multiple of 3 & 5" do
+    expect(fizzbuzz(15)).to eq "Fizzbuzz"
+  end
+
+  it "should return number if number is neither a multiple of 3 or 5" do
+    expect(fizzbuzz(22)).to eq 22
+  end
+end
